cb0eb90e50e0bda15dae9a426ddbe2d3349aa6d2
leehj04 <100088909+leehj04@users.noreply.github.com>
2025-04-11 10:59:40 +0900

    Delete src directory

--- src/main/java/com/example/eweek04a/model/Item.kt
+++
@@ -1,7 +0,0 @@
-package com.example.eweek04a.model
-
-data class Item(
-    val content: String,
-    val time: String,
-    var status: TodoStatus = TodoStatus.PENDING
-)
--- src/main/java/com/example/eweek04a/model/TodoStatus.kt
+++
@@ -1,6 +0,0 @@
-package com.example.eweek04a.model
-
-enum class TodoStatus {
-    PENDING,
-    COMPLETED
-}
